0f05cee06c27542768c860b3c59cec0d4609df41
Asbjørn Pedersen <Asbjrn Pedersen@DESKTOP-PFQ8HJR>
2022-11-27 19:50:42 +0100

    Add project files.

---
+++ NTscam/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NTscam;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NTscam/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace NTscam;
+
+public partial class MainPage : ContentPage
+{
+	int Test = 0;
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+    private void Køb_Clicked(System.Object sender, System.EventArgs e)
+    {
+		Test++;
+    }
+}
+
